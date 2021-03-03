54e6d6f032d4d2deeed63ebaa88533c80a9a05c8
Natsumi <cmcooper123@hotmail.com>
2021-03-03 13:19:56 +1300

    idek at this point

--- html/src/app.js
+++ html/src/app.js
@@ -3701,8 +3701,7 @@ speechSynthesis.getVoices();
                             break;
                         }
                         if ((feedItem.data === ctx.displayName) ||
-                            (((feedItem.type === 'Friend') || (feedItem.type === 'Status')) && (feedItem.displayName === ctx.displayName)) ||
-                            ((feedItem.type === 'GPS') && (feedItem.created_at !== ctx.created_at) && (feedItem.displayName === ctx.displayName))) {
+                            (((feedItem.type === 'Friend') || (feedItem.type === 'Status')) && (feedItem.displayName === ctx.displayName))) {
                             joining = false;
                             break;
                         }
