0aae3dedd10925c2438837d6fa72b0d6cb2a5e78
Natsumi <cmcooper123@hotmail.com>
2021-04-04 23:07:35 +1200

    Release

--- html/src/app.js
+++ html/src/app.js
@@ -3565,7 +3565,7 @@ speechSynthesis.getVoices();
             nextFriendsRefresh: 0,
             isGameRunning: false,
             isGameNoVR: false,
-            appVersion: 'VRCX.PyPyDance.Companion 2021.03.28',
+            appVersion: 'VRCX.PyPyDance.Companion 2021.04.04',
             latestAppVersion: '',
             ossDialog: false,
             exportFriendsListDialog: false,
