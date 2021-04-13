0b771f3600d4c377da4b26e787249cf2f0d33561
Natsumi <cmcooper123@hotmail.com>
2021-04-14 10:40:55 +1200

    Revert temp fix for reporting as active when online

--- html/src/app.js
+++ html/src/app.js
@@ -3297,7 +3297,6 @@ speechSynthesis.getVoices();
                         userId: content.userId
                     }
                 });
-                $app.APILastOnline.set(content.userId, Date.now());
                 break;
 
             case 'friend-active':
@@ -3742,8 +3741,6 @@ speechSynthesis.getVoices();
     };
 
     $app.data.debug = false;
-
-    $app.data.APILastOnline = new Map();
 
     $app.data.sharedFeed = {
         gameLog: {
@@ -5222,15 +5219,6 @@ speechSynthesis.getVoices();
                 });
             }
         } else {
-            if (ctx.state === 'online' && state === 'active') {
-                await new Promise(resolve => setTimeout(resolve, 50000));
-                if (this.APILastOnline.has(id)) {
-                    var date = this.APILastOnline.get(id);
-                    if (date > Date.now() - 60000) {
-                        return;
-                    }
-                }
-            }
             if (ctx.state === 'online') {
                 if (ctx.isVIP) {
                     removeFromArray(this.friendsGroup0_, ctx);
