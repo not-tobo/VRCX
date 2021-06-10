de29ed24d6a988e90fa34adc92adee170332878f
Natsumi <cmcooper123@hotmail.com>
2021-06-10 16:48:32 +1200

    Fix status flapping
    dumb fix for dumb bug

--- html/src/app.js
+++ html/src/app.js
@@ -1187,6 +1187,12 @@ speechSynthesis.getVoices();
             json.$online_for = API.currentUser.$online_for;
             json.$offline_for = API.currentUser.$offline_for;
         }
+        if (typeof json.statusDescription !== 'undefined') {
+            json.statusDescription = $app.replaceBioSymbols(json.statusDescription);
+        }
+        if (typeof json.bio !== 'undefined') {
+            json.bio = $app.replaceBioSymbols(json.bio);
+        }
         if (typeof ref === 'undefined') {
             ref = {
                 id: '',
@@ -13242,6 +13248,46 @@ speechSynthesis.getVoices();
             $app.galleryTable.push(args.json);
         }
     });
+
+    $app.methods.replaceBioSymbols = function (text) {
+        if (!text) {
+            return;
+        }
+        var symbolList = {
+            '@': '＠',
+            '#': '＃',
+            '$': '＄',
+            '%': '％',
+            '&': '＆',
+            '=': '＝',
+            '+': '＋',
+            '/': '⁄',
+            '\\': '＼',
+            ';': ';',
+            ':': '˸',
+            ',': '‚',
+            '?': '？',
+            '!': 'ǃ',
+            '"': '＂',
+            '<': '≺',
+            '>': '≻',
+            '.': '․',
+            '^': '＾',
+            '{': '｛',
+            '}': '｝',
+            '[': '［',
+            ']': '］',
+            '(': '（',
+            ')': '）',
+            '|': '｜',
+            '*': '∗'
+        };
+        for (var key in symbolList) {
+            var regex = new RegExp(symbolList[key], "g");
+            text = text.replace(regex, key);
+        }
+        return text;
+    };
 
     $app = new Vue($app);
     window.$app = $app;
