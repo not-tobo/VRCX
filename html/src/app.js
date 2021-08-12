247bd445eaf12305172702156b96e22efab5fbdc
Natsumi <cmcooper123@hotmail.com>
2021-08-13 11:36:26 +1200

    Fix asset bundle cached needing an int to compare version

--- html/src/app.js
+++ html/src/app.js
@@ -10226,7 +10226,7 @@ speechSynthesis.getVoices();
                 }
             }
             var fileId = extractFileId(assetUrl);
-            var fileVersion = extractFileVersion(assetUrl);
+            var fileVersion = parseInt(extractFileVersion(assetUrl), 10);
             if (fileId) {
                 API.getBundles(fileId)
                     .then((args2) => {
@@ -10641,12 +10641,11 @@ speechSynthesis.getVoices();
                 }
             }
             var fileId = extractFileId(assetUrl);
-            var fileVersion = extractFileVersion(assetUrl);
+            var fileVersion = parseInt(extractFileVersion(assetUrl), 10);
             if (!fileId) {
-                var fileId = extractFileId(ref.assetUrl);
-                var fileVersion = extractFileVersion(ref.assetUrl);
+                fileId = extractFileId(ref.assetUrl);
+                fileVersion = parseInt(extractFileVersion(ref.assetUrl), 10);
             }
-            // var imageId = extractFileId(ref.imageUrl);
             D.fileSize = '';
             if (fileId) {
                 D.fileSize = 'Loading';
@@ -14220,7 +14219,7 @@ speechSynthesis.getVoices();
             }
         }
         var fileId = extractFileId(assetUrl);
-        var fileVersion = extractFileVersion(assetUrl);
+        var fileVersion = parseInt(extractFileVersion(assetUrl), 10);
         if (!fileId) {
             this.downloadCurrent.status = 'Invalid asset url';
             this.downloadCurrent.date = Date.now();
