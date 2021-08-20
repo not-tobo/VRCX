27429b020ca17fd1145faf7586fef8fa66d7d2b2
Natsumi <cmcooper123@hotmail.com>
2021-08-20 22:02:54 +1200

    Fix space at the end of status causing status change event

--- html/src/app.js
+++ html/src/app.js
@@ -14984,7 +14984,7 @@ speechSynthesis.getVoices();
             var regex = new RegExp(symbolList[key], 'g');
             newText = newText.replace(regex, key);
         }
-        return newText.replace(/ {1,}/g, ' ');
+        return newText.replace(/ {1,}/g, ' ').trimRight();
     };
 
     $app.methods.checkCanInvite = function (location) {
