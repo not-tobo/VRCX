9b7718e4bb76f42fbf5e761b19c4cb7b9119ef30
Natsumi <cmcooper123@hotmail.com>
2022-07-13 17:54:33 +1200

    Nightly

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -85,7 +85,7 @@ namespace VRCX
             this.MinimumSize = new System.Drawing.Size(320, 240);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "VRCX";
+            this.Text = "VRCX Nightly";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
             this.Load += new System.EventHandler(this.MainForm_Load);
