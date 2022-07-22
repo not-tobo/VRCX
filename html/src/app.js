b493e78c269c5d8efb74d4d0fb30f7cbf9b23530
Natsumi <cmcooper123@hotmail.com>
2022-07-22 20:04:25 +1200

    Remove emojis fix

--- html/src/app.js
+++ html/src/app.js
@@ -18963,7 +18963,7 @@ speechSynthesis.getVoices();
 
     $app.methods.replaceBioSymbols = function (text) {
         if (!text) {
-            return void 0;
+            return '';
         }
         var symbolList = {
             '@': '＠',
@@ -19003,6 +19003,9 @@ speechSynthesis.getVoices();
     };
 
     $app.methods.removeEmojis = function (text) {
+        if (!text) {
+            return '';
+        }
         return text
             .replace(
                 /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
