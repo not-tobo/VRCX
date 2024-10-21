64ff477ad7cdadba329a2c22338940ed13e65767
Natsumi <cmcooper123@hotmail.com>
2024-10-22 09:12:06 +1300

    Fix launch args

--- html/src/app.js
+++ html/src/app.js
@@ -2937,6 +2937,10 @@ speechSynthesis.getVoices();
                     break retryLoop;
                 } catch (err) {
                     console.error(err);
+                    if (!API.currentUser.isLoggedIn) {
+                        console.error(`User isn't logged in`);
+                        break mainLoop;
+                    }
                     if (err?.message?.includes('Not Found')) {
                         console.error('Awful workaround for awful VRC API bug');
                         break retryLoop;
@@ -25143,7 +25147,11 @@ speechSynthesis.getVoices();
             case -16:
                 if (this.downloadCurrent.ref.id === 'VRCXUpdate') {
                     if (this.downloadCurrent.autoInstall) {
-                        workerTimers.setTimeout(() => this.restartVRCX(), 2000);
+                        var isUpgrade = true;
+                        workerTimers.setTimeout(
+                            () => this.restartVRCX(isUpgrade),
+                            2000
+                        );
                     } else {
                         this.downloadDialog.visible = false;
                         this.pendingVRCXInstall = this.downloadCurrent.ref.name;
@@ -26275,8 +26283,8 @@ speechSynthesis.getVoices();
         }
     };
 
-    $app.methods.restartVRCX = function () {
-        AppApi.RestartApplication();
+    $app.methods.restartVRCX = function (isUpgrade) {
+        AppApi.RestartApplication(isUpgrade);
     };
 
     $app.methods.loadBranchVersions = async function () {
@@ -34043,7 +34051,8 @@ speechSynthesis.getVoices();
                         workerTimers.setTimeout(resolve, 100);
                     });
                     if (action === 'confirm') {
-                        AppApi.RestartApplication();
+                        var isUpgrade = false;
+                        this.restartVRCX(isUpgrade);
                     }
                 }
             }
