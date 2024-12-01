e3bcb38c11eb6a8c43dd87eb170256d42f6c61e9
SemmieDev <semvdmeij124@gmail.com>
2024-12-01 12:00:39 +0100

    Fix and improve auto accept invite requests (#999)

--- html/src/app.js
+++ html/src/app.js
@@ -7378,7 +7378,7 @@ speechSynthesis.getVoices();
         ) {
             return;
         }
-        if (!$utils.checkCanInvite(currentLocation)) {
+        if (!$app.checkCanInvite(currentLocation)) {
             return;
         }
 
@@ -7397,6 +7397,9 @@ speechSynthesis.getVoices();
             )
                 .then((_args) => {
                     $app.$message(`Auto invite sent to ${ref.senderUsername}`);
+                    API.hideNotification({
+                        notificationId: ref.id
+                    });
                     return _args;
                 })
                 .catch((err) => {
