fe9e11e59e919a15a87694d9d9b7f4c32fb82b36
Natsumi <cmcooper123@hotmail.com>
2025-01-26 11:58:32 +1300

    Fix "Group by Instance" disabled causing missing friends

--- src/app.js
+++ src/app.js
@@ -23355,6 +23355,10 @@ console.log(`isLinux: ${LINUX}`);
     };
 
     $app.computed.onlineFriendsByGroupStatus = function () {
+        if (!this.isSidebarGroupByInstance) {
+            return this.onlineFriends;
+        }
+
         const sameInstanceTag = new Set(
             this.friendsInSameInstance.flatMap((item) =>
                 item.map((friend) => friend.ref?.$location.tag)
@@ -23367,6 +23371,10 @@ console.log(`isLinux: ${LINUX}`);
     };
 
     $app.computed.vipFriendsByGroupStatus = function () {
+        if (!this.isSidebarGroupByInstance) {
+            return this.vipFriends;
+        }
+
         const sameInstanceTag = new Set(
             this.friendsInSameInstance.flatMap((item) =>
                 item.map((friend) => friend.ref?.$location.tag)
